55567e7fd55a60d955f6b8e9cadb4a729fe27b9f
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-12 23:24:29 +0300

    feat: init express server

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const PORT = process.env?.PORT ?? 3000;
+
+const app = express();
+
+app.use(morgan("combined"));
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hi!</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Monitor app running at http://localhost:${PORT} 🚀!`);
+});
