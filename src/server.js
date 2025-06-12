8da4f44570821dca14f010e6ee650e4582c0d2f9
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-12 23:33:03 +0300

    chore: add prettier and format files

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const morgan = require("morgan");
+const express = require('express');
+const morgan = require('morgan');
 
 const PORT = process.env?.PORT;
 
 const app = express();
 
-app.use(morgan("combined"));
+app.use(morgan('combined'));
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hi!</h1>");
+app.get('/', (req, res) => {
+  res.send('<h1>Hi!</h1>');
 });
 
 app.listen(PORT, () => {
