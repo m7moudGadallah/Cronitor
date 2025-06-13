88ff2d1a6e06bc278054effcd3b05fd09f77c89d
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-13 21:08:10 +0300

    feat: add monitored-urls routes

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,6 +1,10 @@
 const healthChecksController = require('../controllers/health-checks.controller');
+const monitoredUrlsController = require('../controllers/monitored-urls.controller');
 
 module.exports = app => {
   // HealthChecks Routes
-  app.get('/api/v1/health-checks', healthChecksController.get);
+  app.route('/api/v1/health-checks').get(healthChecksController.get);
+
+  // MonitoredUrls Routes
+  app.route('/api/v1/monitored-urls').post(monitoredUrlsController.create);
 };
