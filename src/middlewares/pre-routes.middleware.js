05675e479466f37ea98de3b37df07a0be4d93266
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-13 20:03:17 +0300

    feat: mount cors middleware

--- src/middlewares/pre-routes.middleware.js
+++ src/middlewares/pre-routes.middleware.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 module.exports = ({ app, staticFilesDir, viewsDir }) => {
+  app.use(cors());
   app.use(morgan(process.env?.ENV == 'dev' ? 'dev' : 'combined'));
   app.set('view engine', 'ejs');
   app.set('views', viewsDir);
