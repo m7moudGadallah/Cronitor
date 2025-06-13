fe4cd0d35d884d18b7ad0acd3fa451d9786906c5
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-13 19:56:35 +0300

    refactor: enhance handlers by passing view file name without ext

--- src/controllers/view.controller.js
+++ src/controllers/view.controller.js
@@ -3,10 +3,10 @@ const healthChecksRepo = require('../repositories/health-checks.repository');
 module.exports = {
   getDashboard: async (_, res) => {
     const healthChecks = await healthChecksRepo.get();
-    res.render('index.ejs', { checks: healthChecks });
+    res.render('index', { checks: healthChecks });
   },
 
   getSettings: async (_, res) => {
-    res.render('settings.ejs', {});
+    res.render('settings', {});
   },
 };
