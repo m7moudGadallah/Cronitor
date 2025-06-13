cfe097965a80931931c9f7372f24a403a971aa3b
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-14 02:55:29 +0300

    refactor: create monitored-urls service

--- src/controllers/monitored-urls.controller.js
+++ src/controllers/monitored-urls.controller.js
@@ -1,14 +1,16 @@
-const monitoredUrlsRepository = require('../repositories/monitored-urls.repository');
+const monitoredUrlsService = require('../services/monitored-urls.service');
 const JsonResponseSchema = require('../utils/json-response-schema.util');
 
 module.exports = {
   get: async (_, res) => {
-    const monitoredUrls = await monitoredUrlsRepository.get();
+    const monitoredUrls = await monitoredUrlsService.getMonitoredUrls();
     res.status(200).json(new JsonResponseSchema({ data: monitoredUrls }));
   },
 
   create: async (req, res) => {
-    const monitoredUrl = await monitoredUrlsRepository.create(req.body);
+    const monitoredUrl = await monitoredUrlsService.createMonitoredUrl(
+      req.body
+    );
     res
       .status(201)
       .json(new JsonResponseSchema({ statusCode: 201, data: monitoredUrl }));
---
+++ src/services/monitored-urls.service.js
@@ -0,0 +1,27 @@
+const monitoredUrlsRepository = require('../repositories/monitored-urls.repository');
+const schedulerService = require('./scheduler.service');
+
+async function getMonitoredUrls() {
+  return await monitoredUrlsRepository.get();
+}
+
+async function createMonitoredUrl({ url, name, interval, isActive }) {
+  const monitoredUrl = await monitoredUrlsRepository.create({
+    data: { url, name, interval, isActive },
+  });
+
+  if (monitoredUrl.isActive) {
+    schedulerService.scheduleUrlCheck({
+      urlId: monitoredUrl.id,
+      url: monitoredUrl.url,
+      interval: monitoredUrl.interval,
+    });
+  }
+
+  return monitoredUrl;
+}
+
+module.exports = {
+  getMonitoredUrls,
+  createMonitoredUrl,
+};
