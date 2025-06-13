c113aa316b53b5981700bb6255af74336e13563f
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-13 18:38:09 +0300

    feat: update morgan to set logger format based on env

--- src/middlewares/pre-routes.middleware.js
+++ src/middlewares/pre-routes.middleware.js
@@ -2,7 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 
 module.exports = ({ app, staticFilesDir, viewsDir }) => {
-  app.use(morgan('combined'));
+  app.use(morgan(process.env?.ENV == 'dev' ? 'dev' : 'combined'));
   app.set('view engine', 'ejs');
   app.set('views', viewsDir);
   app.use(express.static(staticFilesDir));
