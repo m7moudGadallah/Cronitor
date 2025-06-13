2706b6ce56587c69bd77e320baba212a4a3db3a5
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-13 17:41:07 +0300

    feat: add view controller

---
+++ src/controllers/view.controller.js
@@ -0,0 +1,8 @@
+const healthChecksRepo = require('../repositories/health-checks.repository');
+
+module.exports = {
+  getDashboard: async (_, res) => {
+    const healthChecks = await healthChecksRepo.get();
+    res.render('index.ejs', { checks: healthChecks });
+  },
+};
