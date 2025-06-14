d8f7c0112bc2aac84d81b3ee209f311c890d3ac2
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-14 15:44:35 +0300

    feat: update monitored-urls repo to allow sort, pagination, filter options

--- src/repositories/monitored-urls.repository.js
+++ src/repositories/monitored-urls.repository.js
@@ -1,9 +1,22 @@
 const { prisma } = require('../services/prisma.service');
 const normalizeTextUtil = require('../utils/normalize-text.util');
+const BaseRepository = require('./base.repository');
 
-class MonitoredUrlsRepository {
-  async get() {
-    return await prisma.monitoredUrl.findMany();
+class MonitoredUrlsRepository extends BaseRepository {
+  async get({ sort, page, limit, isActive }) {
+    const queryOptions = this._buildQueryOptions({
+      page,
+      limit,
+      sort,
+      filters: { isActive },
+    });
+    if (!queryOptions?.orderBy) {
+      queryOptions.orderBy = {
+        createdAt: 'asc',
+      };
+    }
+
+    return await prisma.monitoredUrl.findMany(queryOptions);
   }
 
   async create({ url, name, interval, isActive }) {
