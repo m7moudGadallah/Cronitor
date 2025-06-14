8d7156206052a516c3a69001e2cd505789128d4a
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-14 17:19:12 +0300

    feat: update json-response-schema util

--- src/utils/json-response-schema.util.js
+++ src/utils/json-response-schema.util.js
@@ -1,7 +1,8 @@
 module.exports = class JsonResponseSchema {
-  constructor({ statusCode = 200, data, message }) {
+  constructor({ statusCode = 200, data, message, errors }) {
     this.success = statusCode < 400;
     this.data = data;
     this.message = message;
+    this.errors = errors;
   }
 };
