48f71081b60b75ff6db3eb46fe7b95a76421f86e
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-14 03:09:44 +0300

    feat: update get health checks method in repo

--- src/repositories/health-checks.repository.js
+++ src/repositories/health-checks.repository.js
@@ -1,44 +1,22 @@
 const { prisma } = require('../services/prisma.service');
 
 class HealthChecksRepository {
-  get() {
-    return Promise.resolve([
-      {
-        timestamp: new Date(Date.now() - 1000 * 60 * 5), // 5 minutes ago
-        url: 'https://google.com',
-        up: true,
-        status: 200,
-        responseTime: 142,
+  async get() {
+    const healthChecks = await prisma.healthCheck.findMany({
+      include: {
+        url: true,
       },
-      {
-        timestamp: new Date(Date.now() - 1000 * 60 * 15), // 15 minutes ago
-        url: 'https://example.com',
-        up: true,
-        status: 200,
-        responseTime: 98,
-      },
-      {
-        timestamp: new Date(Date.now() - 1000 * 60 * 30), // 30 minutes ago
-        url: 'https://my-api-service.com',
-        up: false,
-        status: 503,
-        responseTime: 1200,
-      },
-      {
-        timestamp: new Date(Date.now() - 1000 * 60 * 60), // 1 hour ago
-        url: 'https://test-website.org',
-        up: true,
-        status: 200,
-        responseTime: 210,
-      },
-      {
-        timestamp: new Date(Date.now() - 1000 * 60 * 60 * 2), // 2 hours ago
-        url: 'https://down-for-maintenance.com',
-        up: false,
-        status: 404,
-        responseTime: 320,
-      },
-    ]);
+    });
+
+    return healthChecks.map(healthCheck => ({
+      id: healthCheck.id,
+      url: healthCheck.url.url,
+      urlId: healthCheck.url.id,
+      status: healthCheck.status,
+      up: healthCheck.status < 400,
+      responseTime: healthCheck.responseTime,
+      timestamp: healthCheck.timestamp,
+    }));
   }
 
   async create({ urlId, status, responseTime }) {
