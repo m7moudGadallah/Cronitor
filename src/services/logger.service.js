02fe3d461fe9207742fb8e18be9bcc826ead4cfd
m7moudGadallah <mahmoudgadallah876@gmail.com>
2025-06-14 16:16:35 +0300

    feat: enhnace logger service to handle object logging

--- src/services/logger.service.js
+++ src/services/logger.service.js
@@ -1,27 +1,57 @@
+const util = require('util');
 const chalk = require('chalk');
 
 class Logger {
   #prefix = chalk.gray('[Logger Service]');
 
+  #formatMessage(message) {
+    if (typeof message === 'object') {
+      try {
+        return (
+          '\n' +
+          util.inspect(message, {
+            colors: true,
+            depth: 4,
+            compact: false,
+            showHidden: false,
+          })
+        );
+      } catch (err) {
+        return '\n' + chalk.red('[Unable to inspect object]');
+      }
+    }
+    return message;
+  }
+
   info(message) {
-    console.log(`${this.#prefix} ${chalk.blue(`ℹ ${message}`)}`);
+    console.log(
+      `${this.#prefix} ${chalk.blue(`ℹ ${this.#formatMessage(message)}`)}`
+    );
   }
 
   success(message) {
-    console.log(`${this.#prefix} ${chalk.green(`✓ ${message}`)}`);
+    console.log(
+      `${this.#prefix} ${chalk.green(`✓ ${this.#formatMessage(message)}`)}`
+    );
   }
 
   warning(message) {
-    console.log(`${this.#prefix} ${chalk.yellow(`⚠ ${message}`)}`);
+    console.log(
+      `${this.#prefix} ${chalk.yellow(`⚠ ${this.#formatMessage(message)}`)}`
+    );
   }
 
   error(message) {
-    console.log(`${this.#prefix} ${chalk.red(`✗ ${message}`)}`);
+    console.log(
+      `${this.#prefix} ${chalk.red(`✗ ${this.#formatMessage(message)}`)}`
+    );
   }
 
   debug(message) {
-    if (process.env.NODE_ENV === 'development') {
-      console.log(`${this.#prefix} ${chalk.gray(`🐛 ${message}`)}`);
+    if (process.env?.NODE_ENV === 'dev') {
+      console.log(
+        `${this.#prefix} ${chalk.gray(`🐛 ${this.#formatMessage(message)}`)}`
+      );
     }
   }
 
